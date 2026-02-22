b5ded6177e4654a4836aaa0c25178486d6cf21d2
Rohit Sharma <rs4640808@gmail.com>
2026-02-22 14:23:10 +0530

    some

--- BrackeysGameJam2026.1/Assets/Game/Scripts/SceneSwitcher.cs
+++ BrackeysGameJam2026.1/Assets/Game/Scripts/SceneSwitcher.cs
@@ -1,7 +1,8 @@
 
+using System.Collections;
 using UnityEngine;
 using UnityEngine.SceneManagement;
-public class SceneChanger : MonoBehaviour
+public class SceneSwitcher : MonoBehaviour
 {
    EngineStart engineStart;
 
@@ -17,14 +18,19 @@ public class SceneChanger : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        StartCoroutine(WinConditionMet());
     }
 
-    void WinConditionMet()
-    {
+    IEnumerator WinConditionMet()
+    {   
+        yield return new WaitForSeconds(4f);
+
         if(engineStart.HasStarted())
         {
           SceneManager.LoadScene("MainMenu");
         }
+
+        yield return new WaitForSeconds(4f);
+       
     }
 }
